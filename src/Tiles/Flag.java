cb6c5e6dbfa7ec359de77a08fb947b3923a6dcda
Andrés Navarro <janavarroozuna@gmail.com>
2025-04-05 11:29:11 -0600

    Created the flag class

---
+++ src/Tiles/Flag.java
@@ -0,0 +1,15 @@
+package Tiles;
+
+public class Flag extends Tile{
+
+
+    @Override
+    public void reveal() {
+
+    }
+
+    @Override
+    public boolean ShowTile() {
+        return false;
+    }
+}
